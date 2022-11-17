118876770c9132c32170ce34f906890861141c68
CYNTHIASENGABO <csengabo@macbook-pro.home>
2022-11-17 16:23:59 -0500

    sum

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This program takes the user input of two integer, add them up together then print the result
 
-first_num = input("First: ")
-second_num = input("Second: ")
+first_num = input("First number: ")
+second_num = input("Second number: ")
 
 sum = int(first_num) + int(second_num)
 print("sum: " + str(sum))
